80a3d02324933100304921cf812c1eebd52c062a
tpnet3 <tpnet3@Primary-PC.ZIO_Router>
2016-10-16 20:47:22 +0900

    bug fixed

--- dist/latest/DynamicLoad.js
+++ dist/latest/DynamicLoad.js
@@ -294,9 +294,7 @@ var DynamicLoad;
         Listener.isElementInViewport = function (el) {
             var rect = el.getBoundingClientRect();
             return (rect.top >= 0 &&
-                rect.left >= 0 &&
-                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-                rect.right <= (window.innerWidth || document.documentElement.clientWidth));
+                rect.left >= 0);
         };
         return Listener;
     }());
--- src/Listener.ts
+++ src/Listener.ts
@@ -69,10 +69,17 @@ namespace DynamicLoad {
 
             return (
                 rect.top >= 0 &&
-                rect.left >= 0 &&
-                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
-                rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+                rect.left >= 0
             );
+            
+            /*
+            return (
+                rect.top >= 0 &&
+                rect.left >= 0 &&
+                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+            );
+            */
         }
     }
 }
